02185fd67d9ff943a90d5cb96ca6641df35450a9
lakshyasinghrajput <solankilakshaya@gmail.com>
2026-04-01 00:31:32 +0530

    added matches POST & GET endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { matchRouter } from './routes/matches';
 
 const app = express();
 const port = process.env.PORT;
@@ -8,6 +9,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
   console.log('Match list')
 })
+
+app.use('/matches', matchRouter)
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`)
